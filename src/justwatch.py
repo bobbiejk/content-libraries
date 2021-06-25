5b2d6cd6d75ab69cfed85340b576dde008e68f27
B.J. Krijger <B.J.Krijger@tilburguniversity.edu>
2021-06-25 16:45:06 +0200

    added url

---
+++ src/justwatch.py
@@ -0,0 +1,25 @@
+
+
+
+
+# get url to scrape from
+base_url = "https://www.justwatch.com" 
+location_url = "/us/"
+provider_url = "new?providers="
+
+service_keys = {"amazon":"amp",
+                "apple":"atp",
+                "disney":"dnp",
+                "discovery":"dpu",
+                "hbo":"hbm",
+                "hulu":"hlu",
+                "netflix":"nfx"}
+
+service_abbreviations = service_keys.keys()
+
+service_url = "" 
+for service in service_abbreviations:
+    service_url = service_url + "," + service
+
+url = base_url + location_url + provider_url + service_url
+
