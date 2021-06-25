7490e5c05f33d9bc4ab0b9e6ac4746fce53026de
B.J. Krijger <B.J.Krijger@tilburguniversity.edu>
2021-06-25 16:47:53 +0200

    set up selenium

--- src/justwatch.py
+++ src/justwatch.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 from time import sleep
 import requests
+import selenium
 
 # get url to scrape from
 base_url = "https://www.justwatch.com" 
@@ -24,4 +25,11 @@ for service in service_abbreviations:
 
 url = base_url + location_url + provider_url + service_url
 
-s
+# set up selenium
+driver = webdriver.Chrome()
+
+driver.get(url)
+sleep(2)
+request = driver.page_source.encode("utf-8")
+soup = BeautifulSoup(request, "html.parser")
+
