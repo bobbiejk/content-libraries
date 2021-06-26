0873a86a39521382226eba1fd8d9ab64a876b398
B.J. Krijger <B.J.Krijger@tilburguniversity.edu>
2021-06-26 11:38:07 +0200

    scrolls for each bar

--- src/justwatch.py
+++ src/justwatch.py
@@ -7,7 +7,8 @@ from time import sleep
 import requests
 from selenium import webdriver
 import regex as re
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
 # get url to scrape from
 base_url = "https://www.justwatch.com" 
@@ -35,6 +36,7 @@ url = base_url + location_url + provider_url + service_url
 
 # set up selenium
 driver = webdriver.Chrome()
+print(type(driver))
 
 driver.get(url)
 sleep(2)
@@ -44,15 +46,20 @@ soup = BeautifulSoup(request, "html.parser")
 
 # get the timeline
 timeline = soup.find(class_ = "timeline").find_all(class_= re.compile("timeline__provider-block timeline__timeframe--"))
+timeline_driver = driver.find_element_by_class_name("timeline")
+time_items_driver = timeline_driver.find_elements_by_class_name("timeline__provider-block")
 
 content_library = []
+
+counter = 0
 
 for time_item in timeline:
     
     nr_releases = time_item.get_text().split(" ")[1]
-    nr_scrols = int(nr_releases)/8
+    nr_scrols = round(int(nr_releases)/8)
     date = time_item.attrs["class"][1][21:31]
     service = time_item.find("img").attrs["alt"]
+    print(f"There are {nr_releases} on {date} for service {service}. Need to scroll {nr_scrols} times..")
     
     scrolled = 0
     
@@ -60,26 +67,34 @@ for time_item in timeline:
         
         titles = time_item.find_all("a")
 
-        for item in titles:
-            try:
-                item_url = item.attrs["href"]
-            except:
-                print('Scrolling to the left to collect more titles')
-                scroll = driver.find_element_by_class_name("hidden-horizontal-scrollbar__nav")  
-                print(scroll)          
-                scroll.click()
-                scrolled += 1
-                break            
-        
-        content_library.append({"service": service,
-                                "date": date,
-                                "nr_releases": nr_releases,
-                               "url": base_url + item_url})
-        
-        
-        
-print(content_library)
+        for item in range(len(titles)):
 
+            if item < 8:
+                try:
+                    item_url = titles[item].attrs["href"]
+                except:
+                    print(f"Didn't work out for {titles[item]}")
 
+            if item > 7:
+                    print('Scrolling to the left to collect more titles')
+                    element_to_hover_over = time_items_driver[counter]
+                    hover = ActionChains(driver).move_to_element(element_to_hover_over)
+                    hover.perform()
+                    sleep(2)
+                    scroll = time_items_driver[counter].find_element_by_class_name("hidden-horizontal-scrollbar__nav")  
+     
+                    scroll.click()
+                    scrolled += 1
+                    break    
+            
+            content_library.append({"service": service,
+                                    "date": date,
+                                    "nr_releases": nr_releases,
+                                    "url": base_url + item_url})
 
+        scrolled += 1 
+    
+    print(content_library)
+    counter += 1 
+              
 # %%
